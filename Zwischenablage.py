06fcfd35409b46ec3c263f24eef35c767851658c
grigory-consulting <consulting@vadze.de>
2026-09-23 10:44:09 +0200

    Implement data visualization for training samples
    Added code to visualize training data with matplotlib.

--- Zwischenablage.py
+++ Zwischenablage.py
@@ -12,3 +12,15 @@ test_data = datasets.FashionMNIST(root="./data", train=False, download=True, tra
 train_loader = torch.utils.data.DataLoader(train_data, batch_size= 64, shuffle=True) # shuffle = Randomisieren, 
 test_loader = torch.utils.data.DataLoader(test_data, batch_size = 64, shuffle=False) #
 
+
+
+import matplotlib.pyplot as plt
+klassen = ["T-Shirt/Top", "Hose", "Pullover", "Kleid", "Mantel",
+           "Sandale", "Hemd", "Sneaker", "Tasche", "Stiefelette"]
+fig, axes = plt.subplots(2, 5, figsize=(10, 4.5))
+for nummer, ax in enumerate(axes.flat):
+    index = int((train_data.targets == nummer).nonzero()[0])
+    ax.imshow(train_data[index][0][0], cmap="gray")
+    ax.set_title(f"{nummer}: {klassen[nummer]}")
+    ax.axis("off")
+plt.show()
